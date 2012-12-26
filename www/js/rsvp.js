32970a43d97ee47c293d6f3cd17cc6cfdad5d5ef
Benjamin Bellamy <benjamin.bellamy@keeward.com>
2012-12-26 03:00:08 +0300

    deleted:    cgi-bin/getpid.txt 	modified:   cgi-bin/reboot.txt 	modified:   cgi-bin/varnishstat.json 	modified:   www/css/rsvp.css 	modified:   www/index.html 	modified:   www/js/rsvp.js 	modified:   www/logs.html

--- www/js/rsvp.js
+++ www/js/rsvp.js
@@ -1,10 +1,4 @@
-function callVarnish(url) {
-	$.get(url, function(data) {
-		modalMessageBox(data);
-	});	
-}
-
-	
+/* Displays a message */
 function modalMessageBox(message) {
 	if(message.trim()==''){ 
 		message='Hmmm, no, nothing to say…';
@@ -13,7 +7,40 @@ function modalMessageBox(message) {
 	$('#ServerMessage').modal();
 }
 
+/* Call a cgi-bin functio then displays the returned valu in modal box */
+function callVarnish(url) {
+	$.get(url, function(data) {
+		modalMessageBox(data);
+	});	
+}
+
+/* Update statistics table */
 function updateStat() {
+	// Get varnishd pid
+	// ps aux | grep varnishd | grep -v grep | awk '{print $2}'
+	$.get('/cgi-bin/getvrnpid.txt', function(pid) {
+	// In case Varnishd is not running:
+	if(pid.trim()==''){
+		$('#pid').text('Stopped');
+		$('#pid').removeClass('badge-success');
+	// Varnishd is running:
+	}else{
+		$('#pid').text('Varnish Running ('+pid.trim()+')');
+		$('#pid').addClass('badge-success');	
+	
+		// Get varnish status
+		// varnishadm -S /etc/varnish/secret.www-data -T 127.0.0.1:6082 status
+			$.get('/cgi-bin/status.txt', function(status) {
+	// varnish is stopped:
+	if(status.indexOf('running')==-1){
+		$('#status').text('Stopped');
+		$('#status').removeClass('badge-success');
+	// varnish has started:
+	}else{	
+			$('#status').text(status.trim());
+		$('#status').addClass('badge-success');	
+	// get varnishstat
+   //	varnishstat -j -1
 	$.getJSON('/cgi-bin/varnishstat.json', function(data) {
 		// for each value:
 		$.each(data, function(key, val) {
@@ -29,59 +56,94 @@ function updateStat() {
 					$('#s_'+ekey+' > td:last').html('<label>'+val['value']+'</label>');
 				// else we add a new row to the html table:
 				}else{
-					$('#stat').append( '<tr id="s_'+ekey+'"><td><a href="#" class="key" rel="tooltip" title="'+val['description']+'">' + key + '</a></td><td>' + val['value'] + '</td></tr>');
+					$('#stat').append( '<tr id="s_'+ekey+'" class="secondary"><td><a href="#" class="key" rel="tooltip" title="'+val['description']+'">' + key + '</a></td><td>' + val['value'] + '</td></tr>');
 				}
 		});
+		// we calculate the ratio HIT/(HIT+MISS)
+		ratio=Math.floor(data['cache_hit']['value'] / (data['cache_miss']['value'] + data['cache_hit']['value']) * 100);
+		// Depending on the ratio we change the badge color:
+		if(isNaN(ratio)){
+			$('#ratio').removeClass('badge-inverse badge-important badge-warning badge-success');
+		} else if(ratio<15){
+			$('#ratio').removeClass('badge-inverse badge-important badge-warning badge-success').addClass('badge-inverse');
+		} else if(ratio<50){
+			$('#ratio').removeClass('badge-inverse badge-important badge-warning badge-success').addClass('badge-important');
+		} else if(ratio<90){
+			$('#ratio').removeClass('badge-inverse badge-important badge-warning badge-success').addClass('badge-warning');
+		} else {
+			$('#ratio').removeClass('badge-inverse badge-important badge-warning badge-success').addClass('badge-success');
+		}
+		$('#ratio').text(ratio+"%");
 		$('.key').tooltip();
 	});
+	}
+	// come back in 1 second:
 	setTimeout(updateStat, 1000);
+	});
+	}
+		});
 }
 
+/* Ban everything */
 function banCache() {
 	callVarnish('/cgi-bin/ban.txt');
 }
 
-
+/* Stops the starts Varnish */
 function rebootVarnish() {
 	callVarnish('/cgi-bin/reboot.txt');	
 }
 
-
+/* Starts logging */
 function startLog(){
 	callVarnish('/cgi-bin/startlog.txt');
 }
 
+/* Stops logging */
 function stopLog(){
 	callVarnish('/cgi-bin/stoplog.txt');
 }
 
+/* Stops all logging processes */
 function stopAll(){
 	callVarnish('/cgi-bin/stopall.txt');
 }
 
+/* Empties log file */
 function clearLog(){
 	callVarnish('/cgi-bin/clearlog.txt');
 }
 
+/* update log */
 function updateLog() {
-	$.get('/cgi-bin/getpid.txt', function(pid) {
+	// Gets log process pid:
+	$.get('/cgi-bin/getlogpid.txt', function(pid) {
+	// if no log process is running for this public ip:
 	if(pid.trim()==''){
-		$('#status').text('Stopped');
-		$('#status').removeClass('badge-success');
+		$('#pid').text('Stopped');
+		$('#pid').removeClass('badge-success');
+		// Gets log in case there is an old one worth reading:
+		if($('#logs').text()==''){
+			getLog();
+		}
+	// a log process is currently running:
 	}else{
-		$('#status').text('Running '+pid);
-		$('#status').addClass('badge-success');
-		$.get('/cgi-bin/readlog.txt', function(data) {
-			$('#logs').text(data);
-			$("#logs").animate({ scrollTop: $('#logs')[0].scrollHeight}, 1000);
-		});
+		$('#pid').text('Log Running ('+pid.trim()+')');
+		$('#pid').addClass('badge-success');
+      // get log content:
+		getLog();
 	}
+	// come back in 2 seconds:
 	setTimeout(updateLog, 2000);
 	});
 }
-	
-	
-	
+/* Gets log content and scrolls down */
+function getLog(){
+	$.get('/cgi-bin/readlog.txt', function(data) {
+		$('#logs').text(data);
+		$("#logs").animate({ scrollTop: $('#logs')[0].scrollHeight}, 1000);
+	});
+}
 	
 	
 	
